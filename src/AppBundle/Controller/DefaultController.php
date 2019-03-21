0f4044bd79043bcc6c4731a8432f4cf94b490266
LyamkinViktor <Viktor.Lyamkin1.0@gmail.com>
2019-03-21 21:02:56 +0700

    first commit

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $book = 'php the right way';
+
+        return $this->render('default/index.html.twig', ['book'=> $book]);
+    }
+
+    /**
+     * @Route("/feedback", name="feedback")
+     */
+    public function feedbackAction()
+    {
+        return new Response('<!doctype html><body>It works!</body></html5>');
+    }
+}
